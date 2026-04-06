b010f0b58b3c83ae255d550f6642999c5d781876
thereWolf <bdunham484@gmail.com>
2026-04-06 13:20:32 -0500

    Add Antone's to the list of Austin venues in Control component

--- client/src/pages/Control.js
+++ client/src/pages/Control.js
@@ -124,7 +124,7 @@ const Control = () => {
     //     }
     // }, [isScraperLoading, totalScraped, updateScrapeMeta]);
 
-    const austinVenues = ['The 13th Floor', '29th Street Ballroom', '3TEN Austin City Limits Live', 'The ABGB'];
+    const austinVenues = ['13th Floor', '29th Street Ballroom', '3TEN Austin City Limits Live', 'ABGB', "Antone's"];
 
     const getVenueLightClass = (venue) => {
         const status = venueStatuses?.[venue];
