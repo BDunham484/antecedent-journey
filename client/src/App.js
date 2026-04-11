713062991af6d4307f29f5959002abe96b6308a9
thereWolf <bdunham484@gmail.com>
2026-04-11 14:23:17 -0500

    chore: delete unused App.css and remove its import

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
